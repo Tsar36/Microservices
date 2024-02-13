5624c4b625450a2b7cf8ec5187cdfb5e87eec961
TsaR <seriy.tsar@gmail.com>
2024-02-13 11:25:44 +0200

    upd volumes script

--- Kubernetes/configs/Deployments/volumes/aws_delete_volumes.sh
+++ Kubernetes/configs/Deployments/volumes/aws_delete_volumes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "To delete a specific volume use: 'aws ec2 delete-volume --volume-id <vol-ids>' "
+sleep 5
+
 echo "Delete all volumes"
 
 aws ec2 describe-volumes | egrep "VolumeId" | awk '{print $NF}' | sed 's/\"//g;s/\,//' > vol_ids
